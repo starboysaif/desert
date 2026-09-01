ed1932ed0462196f2ae3bb78ee901b5c63443769
starboysaif <saiffmetwalli@gmail.com>
2026-09-01 19:47:37 +0300

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,7 +2,10 @@ import Image from "next/image";
 
 export default function Logo({ size = 60 }: { size?: number }) {
   return (
-    <div style={{ width: size, height: size, position: "relative" }}>
+    <div
+      style={{ width: size, height: size, position: "relative" }}
+      className="tumbleweed-roll"
+    >
       <Image
         src="/logo.png"
         alt="Desert logo"
