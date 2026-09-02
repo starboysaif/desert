6f8176109244d2a23f6690bf0c66b6921d9e1c27
starboysaif <saiffmetwalli@gmail.com>
2026-09-02 04:25:45 +0300

    Create Enter3DButton.tsx

---
+++ components/Enter3DButton.tsx
@@ -0,0 +1,15 @@
+export default function Enter3DButton() {
+  return (
+    <iframe
+      src="/enter-button-3d.html"
+      style={{
+        width: 420,
+        height: 160,
+        maxWidth: "80vw",
+        border: "none",
+        background: "transparent",
+      }}
+      title="3D Enter Button"
+    />
+  );
+}
